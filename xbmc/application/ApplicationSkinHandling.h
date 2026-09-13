be2b566346417b34cbf70d04aea64ba03acc57b5
aqrfcx <aqrfcx@users.noreply.github.com>
2026-09-13 19:25:02 +0330

    Import Kodi files batch 217

---
+++ xbmc/application/ApplicationSkinHandling.h
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (C) 2005-2026 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "application/IApplicationComponent.h"
+
+#include <string>
+
+class CApplication;
+class CSetting;
+class IMsgTargetCallback;
+class IWindowManagerCallback;
+
+/*!
+ * \brief Class handling application support for skin management.
+ */
+class CApplicationSkinHandling : public IApplicationComponent
+{
+  friend class CApplication;
+
+public:
+  CApplicationSkinHandling(IMsgTargetCallback* msgCb,
+                           IWindowManagerCallback* wCb,
+                           bool& bInitializing);
+
+  void UnloadSkin();
+
+  bool OnSettingChanged(const CSetting& setting);
+  void ReloadSkin(bool confirm = false);
+  bool LoadSkin(const std::string& skinID);
+
+protected:
+  bool LoadCustomWindows();
+
+  /*!
+ * \brief Called by the application main/render thread for processing
+ *        operations belonging to the skin.
+ */
+  void ProcessSkin() const;
+
+  /*!
+   * \brief Run a skin reload that was deferred out of a nested render loop.
+   *
+   * Does nothing unless a reload is pending and the render loop has unwound.
+   */
+  void ProcessPendingSkinReload();
+
+  bool m_saveSkinOnUnloading = true;
+  bool m_confirmSkinChange = true;
+  bool m_ignoreSkinSettingChanges = false;
+  bool m_pendingSkinReload = false;
+  bool m_pendingSkinReloadConfirm = false;
+  IMsgTargetCallback* m_msgCb;
+  IWindowManagerCallback* m_wCb;
+  bool& m_bInitializing;
+};
