2ad66b96f30d2252b8ad5c6ea2aee3874e73ed0a
aqrfcx <aqrfcx@users.noreply.github.com>
2026-09-13 19:25:15 +0330

    Import Kodi files batch 240

---
+++ xbmc/cores/VideoPlayer/Buffers/VideoBufferDRMPRIME.h
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (C) 2017-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "cores/VideoPlayer/Buffers/VideoBuffer.h"
+#include "cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodec.h"
+
+extern "C"
+{
+#include <libavutil/frame.h>
+#include <libavutil/hwcontext_drm.h>
+}
+
+namespace DRMPRIME
+{
+
+std::string GetColorEncoding(const VideoPicture& picture);
+std::string GetColorRange(const VideoPicture& picture);
+
+} // namespace DRMPRIME
+
+class CVideoBufferDRMPRIME : public CVideoBuffer
+{
+public:
+  CVideoBufferDRMPRIME() = delete;
+  ~CVideoBufferDRMPRIME() override = default;
+
+  virtual void SetPictureParams(const VideoPicture& picture) { m_picture.SetParams(picture); }
+  virtual const VideoPicture& GetPicture() const { return m_picture; }
+  virtual uint32_t GetWidth() const { return GetPicture().iWidth; }
+  virtual uint32_t GetHeight() const { return GetPicture().iHeight; }
+
+  virtual AVDRMFrameDescriptor* GetDescriptor() const = 0;
+  virtual bool IsValid() const;
+  virtual bool AcquireDescriptor() { return true; }
+  virtual void ReleaseDescriptor() {}
+
+protected:
+  explicit CVideoBufferDRMPRIME(int id);
+
+  VideoPicture m_picture;
+};
+
+class CVideoBufferDRMPRIMEFFmpeg : public CVideoBufferDRMPRIME
+{
+public:
+  CVideoBufferDRMPRIMEFFmpeg(IVideoBufferPool& pool, int id);
+  ~CVideoBufferDRMPRIMEFFmpeg() override;
+  void SetRef(AVFrame* frame);
+  void Unref();
+
+  AVDRMFrameDescriptor* GetDescriptor() const override
+  {
+    return reinterpret_cast<AVDRMFrameDescriptor*>(m_pFrame->data[0]);
+  }
+
+protected:
+  AVFrame* m_pFrame = nullptr;
+};
+
+class CVideoBufferPoolDRMPRIMEFFmpeg : public IVideoBufferPool
+{
+public:
+  ~CVideoBufferPoolDRMPRIMEFFmpeg() override;
+  void Return(int id) override;
+  CVideoBufferDRMPRIMEFFmpeg* Get() override;
+
+protected:
+  CCriticalSection m_critSection;
+  std::vector<CVideoBufferDRMPRIMEFFmpeg*> m_all;
+  std::deque<int> m_used;
+  std::deque<int> m_free;
+};
---
+++ xbmc/cores/VideoPlayer/Buffers/VideoBufferPoolDMA.cpp
@@ -0,0 +1,186 @@
+/*
+ *  Copyright (C) 2005-2020 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "VideoBufferPoolDMA.h"
+
+#include "cores/VideoPlayer/Buffers/VideoBufferDMA.h"
+#include "utils/BufferObjectFactory.h"
+
+#include <mutex>
+
+#include <drm_fourcc.h>
+
+extern "C"
+{
+#include <libavutil/pixdesc.h>
+#include <libavutil/pixfmt.h>
+}
+
+CVideoBufferPoolDMA::~CVideoBufferPoolDMA()
+{
+  std::unique_lock lock(m_critSection);
+
+  for (auto buf : m_all)
+    delete buf;
+}
+
+CVideoBufferDMA* CVideoBufferPoolDMA::Get()
+{
+  std::unique_lock lock(m_critSection);
+
+  CVideoBufferDMA* buf = nullptr;
+  if (!m_free.empty())
+  {
+    int idx = m_free.front();
+    m_free.pop_front();
+    m_used.push_back(idx);
+    buf = m_all[idx];
+  }
+  else
+  {
+    int id = m_all.size();
+    buf = new CVideoBufferDMA(id, m_fourcc, m_planes, m_size);
+
+    if (!buf->Alloc())
+    {
+      delete buf;
+      return nullptr;
+    }
+
+    m_all.push_back(buf);
+    m_used.push_back(id);
+  }
+
+  buf->Acquire(GetPtr());
+  return buf;
+}
+
+void CVideoBufferPoolDMA::Return(int id)
+{
+  std::unique_lock lock(m_critSection);
+
+  auto it = m_used.begin();
+  while (it != m_used.end())
+  {
+    if (*it == id)
+    {
+      m_used.erase(it);
+      break;
+    }
+    else
+      ++it;
+  }
+  m_free.push_back(id);
+}
+
+void CVideoBufferPoolDMA::Configure(AVPixelFormat format, int size)
+{
+  std::unique_lock lock(m_critSection);
+
+  m_fourcc = TranslateFormat(format);
+  const int planeCount = av_pix_fmt_count_planes(format);
+  m_planes = planeCount > 0 ? static_cast<uint32_t>(planeCount) : 0;
+  m_size = static_cast<uint64_t>(size);
+}
+
+bool CVideoBufferPoolDMA::IsConfigured()
+{
+  std::unique_lock lock(m_critSection);
+
+  return (m_fourcc != 0 && m_size != 0);
+}
+
+bool CVideoBufferPoolDMA::IsCompatible(AVPixelFormat format, int size)
+{
+  std::unique_lock lock(m_critSection);
+
+  if (m_fourcc != TranslateFormat(format) || m_size != static_cast<uint64_t>(size))
+    return false;
+
+  return true;
+}
+
+void CVideoBufferPoolDMA::Released(CVideoBufferManager& videoBufferManager)
+{
+  if (!CBufferObjectFactory::CreateBufferObject(true))
+    return;
+
+  videoBufferManager.RegisterPool(std::make_shared<CVideoBufferPoolDMA>());
+}
+
+std::shared_ptr<IVideoBufferPool> CVideoBufferPoolDMA::CreatePool()
+{
+  return std::make_shared<CVideoBufferPoolDMA>();
+}
+
+uint32_t CVideoBufferPoolDMA::TranslateFormat(AVPixelFormat format)
+{
+  switch (format)
+  {
+    case AV_PIX_FMT_NV12:
+      return DRM_FORMAT_NV12;
+    case AV_PIX_FMT_P010:
+      return DRM_FORMAT_P010;
+    case AV_PIX_FMT_YUV420P:
+    case AV_PIX_FMT_YUVJ420P:
+      return DRM_FORMAT_YUV420;
+    case AV_PIX_FMT_YUV422P:
+    case AV_PIX_FMT_YUVJ422P:
+      return DRM_FORMAT_YUV422;
+    case AV_PIX_FMT_YUYV422:
+      return DRM_FORMAT_YUYV;
+    case AV_PIX_FMT_UYVY422:
+      return DRM_FORMAT_UYVY;
+    case AV_PIX_FMT_YUV444P:
+    case AV_PIX_FMT_YUVJ444P:
+      return DRM_FORMAT_YUV444;
+      //! @todo remove #ifdef guards when libdrm >= 2.4.125 is the minimum
+#if defined(DRM_FORMAT_S010)
+    case AV_PIX_FMT_YUV420P10:
+      return DRM_FORMAT_S010;
+#endif
+#if defined(DRM_FORMAT_S012)
+    case AV_PIX_FMT_YUV420P12:
+      return DRM_FORMAT_S012;
+#endif
+#if defined(DRM_FORMAT_S210)
+    case AV_PIX_FMT_YUV422P10:
+      return DRM_FORMAT_S210;
+#endif
+#if defined(DRM_FORMAT_S212)
+    case AV_PIX_FMT_YUV422P12:
+      return DRM_FORMAT_S212;
+#endif
+#if defined(DRM_FORMAT_S410)
+    case AV_PIX_FMT_YUV444P10:
+      return DRM_FORMAT_S410;
+#endif
+#if defined(DRM_FORMAT_S412)
+    case AV_PIX_FMT_YUV444P12:
+      return DRM_FORMAT_S412;
+#endif
+    // Packed RGB formats. AV_PIX_FMT_BGR0 = bytes B,G,R,X = DRM_FORMAT_XRGB8888.
+    case AV_PIX_FMT_BGR0:
+      return DRM_FORMAT_XRGB8888;
+    case AV_PIX_FMT_X2RGB10LE:
+      return DRM_FORMAT_XRGB2101010;
+      // ffmpeg has no X variant of 16-bit RGB; we use RGBA64/RGBAF16 with the
+      // alpha component treated as undefined padding (X). DRM 16-bit RGB
+      // formats need libdrm >= 2.4.110.
+#if defined(DRM_FORMAT_XRGB16161616)
+    case AV_PIX_FMT_RGBA64LE:
+      return DRM_FORMAT_XRGB16161616;
+#endif
+#if defined(DRM_FORMAT_XRGB16161616F)
+    case AV_PIX_FMT_RGBAF16LE:
+      return DRM_FORMAT_XRGB16161616F;
+#endif
+    default:
+      return 0;
+  }
+}
