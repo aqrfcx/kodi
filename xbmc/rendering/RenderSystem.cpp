c62ad6bbf2cb458671b4b295f1e53d2e51350223
aqrfcx <aqrfcx@users.noreply.github.com>
2026-09-13 19:26:15 +0330

    Import Kodi files batch 398

---
+++ xbmc/rendering/RenderSystem.cpp
@@ -0,0 +1,155 @@
+/*
+ *  Copyright (C) 2005-2026 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "RenderSystem.h"
+
+#include "ServiceBroker.h"
+#include "Util.h"
+#include "guilib/GUIFontManager.h"
+#include "guilib/GUIImage.h"
+#include "guilib/GUILabelControl.h"
+#include "settings/AdvancedSettings.h"
+#include "settings/SettingsComponent.h"
+#include "windowing/WinSystem.h"
+
+#include <memory>
+
+unsigned int CRenderSystemBase::m_GUIElementCount = 0;
+
+CRenderSystemBase::CRenderSystemBase()
+{
+  OnAdvancedSettingsLoaded();
+
+  const auto advSettings{CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()};
+
+  m_settingsCallbackHandle =
+      advSettings->RegisterSettingsLoadedCallback([this]() { OnAdvancedSettingsLoaded(); });
+}
+
+void CRenderSystemBase::OnAdvancedSettingsLoaded()
+{
+  const auto advSettings{CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()};
+
+  std::unique_lock lock(m_settingsSection);
+
+  m_showSplashImage = advSettings->m_splashImage;
+  m_guiFrontToBackRendering = advSettings->m_guiFrontToBackRendering;
+  m_guiGeometryClear =
+      advSettings->m_guiGeometryClear ? ClearFunction::GEOMETRY : ClearFunction::FIXED_FUNCTION;
+}
+
+CRenderSystemBase::~CRenderSystemBase()
+{
+  const auto advSettings{CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()};
+
+  if (m_settingsCallbackHandle.has_value() && advSettings != nullptr)
+    advSettings->UnregisterSettingsLoadedCallback(m_settingsCallbackHandle.value());
+}
+
+void CRenderSystemBase::GetRenderVersion(unsigned int& major, unsigned int& minor) const
+{
+  major = m_RenderVersionMajor;
+  minor = m_RenderVersionMinor;
+}
+
+bool CRenderSystemBase::SupportsNPOT(bool dxt) const
+{
+  if (dxt)
+    return false;
+
+  return true;
+}
+
+bool CRenderSystemBase::SupportsStereo(RenderStereoMode mode) const
+{
+  switch(mode)
+  {
+    case RenderStereoMode::OFF:
+    case RenderStereoMode::SPLIT_HORIZONTAL:
+    case RenderStereoMode::SPLIT_VERTICAL:
+    case RenderStereoMode::MONO:
+      return true;
+    default:
+      return false;
+  }
+}
+
+void CRenderSystemBase::ShowSplash(const std::string& message)
+{
+  if (!GetShowSplashImage() && !(m_splashImage || !message.empty()))
+    return;
+
+  if (!m_splashImage)
+  {
+    m_splashImage = std::make_unique<CGUIImage>(
+        0, 0, .0f, .0f,
+        static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth()),
+        static_cast<float>(CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight()),
+        CTextureInfo(CUtil::GetSplashPath()));
+    m_splashImage->SetAspectRatio(CAspectRatio::SCALE);
+  }
+
+  CServiceBroker::GetWinSystem()->GetGfxContext().lock();
+  CServiceBroker::GetWinSystem()->GetGfxContext().Clear(0xff000000);
+
+  RESOLUTION_INFO res = CServiceBroker::GetWinSystem()->GetGfxContext().GetResInfo();
+  CServiceBroker::GetWinSystem()->GetGfxContext().SetRenderingResolution(res, true);
+
+  //render splash image
+  BeginRender();
+
+  m_splashImage->AllocResources();
+  m_splashImage->Render();
+  m_splashImage->FreeResources();
+
+  if (!message.empty())
+  {
+    if (!m_splashMessageLayout)
+    {
+      auto messageFont = g_fontManager.LoadTTF("__splash__", "arial.ttf", 0xFFFFFFFF, 0, 40,
+                                               FONT_STYLE_NORMAL, false, 1.0f, 1.0f, &res);
+      if (messageFont)
+        m_splashMessageLayout = std::make_unique<CGUITextLayout>(messageFont, true, .0f);
+    }
+
+    if (m_splashMessageLayout)
+    {
+      m_splashMessageLayout->Update(message, 1150, false, true);
+      float textWidth, textHeight;
+      m_splashMessageLayout->GetTextExtent(textWidth, textHeight);
+
+      int width = CServiceBroker::GetWinSystem()->GetGfxContext().GetWidth();
+      int height = CServiceBroker::GetWinSystem()->GetGfxContext().GetHeight();
+      float y = height - textHeight - 100;
+      m_splashMessageLayout->RenderOutline(width/2, y, 0, 0xFF000000, XBFONT_CENTER_X, width);
+    }
+  }
+
+  //show it on screen
+  EndRender();
+  CServiceBroker::GetWinSystem()->GetGfxContext().unlock();
+  CServiceBroker::GetWinSystem()->GetGfxContext().Flip(true, false);
+}
+
+bool CRenderSystemBase::GetShowSplashImage()
+{
+  std::unique_lock lock(m_settingsSection);
+  return m_showSplashImage;
+}
+
+bool CRenderSystemBase::GetEnabledFrontToBackRendering()
+{
+  std::unique_lock lock(m_settingsSection);
+  return m_guiFrontToBackRendering;
+}
+
+ClearFunction CRenderSystemBase::GetClearFunction()
+{
+  std::unique_lock lock(m_settingsSection);
+  return m_guiGeometryClear;
+}
---
+++ xbmc/rendering/RenderSystem.h
@@ -0,0 +1,138 @@
+/*
+ *  Copyright (C) 2005-2026 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "RenderSystemTypes.h"
+#include "threads/CriticalSection.h"
+#include "utils/ColorUtils.h"
+#include "utils/Geometry.h"
+
+#include <memory>
+#include <optional>
+#include <string>
+
+/*
+ *   CRenderSystemBase interface allows us to create the rendering engine we use.
+ *   We currently have two engines: OpenGL and DirectX
+ *   This interface is very basic since a lot of the actual details will go in to the derived classes
+ */
+
+enum class DepthCulling
+{
+  OFF,
+  BACK_TO_FRONT,
+  FRONT_TO_BACK,
+};
+
+enum class ClearFunction
+{
+  FIXED_FUNCTION,
+  GEOMETRY,
+};
+
+class CGUIImage;
+class CGUITextLayout;
+
+class CRenderSystemBase
+{
+public:
+  CRenderSystemBase();
+  virtual ~CRenderSystemBase();
+
+  // Number of GUI elements (textures, text, overlays) drawn this frame; reset
+  // in BeginRender, bumped at each GUI draw primitive. Render-thread only.
+  static unsigned int m_GUIElementCount;
+
+  unsigned int GetGUIElementCount() const { return m_GUIElementCount; }
+
+  virtual bool InitRenderSystem() = 0;
+  virtual bool DestroyRenderSystem() = 0;
+  virtual bool ResetRenderSystem(int width, int height) = 0;
+
+  virtual bool BeginRender() = 0;
+  virtual bool EndRender() = 0;
+  virtual void PresentRender(bool rendered, bool videoLayer) = 0;
+  virtual void InvalidateColorBuffer() {}
+  virtual bool ClearBuffers(KODI::UTILS::COLOR::Color color) = 0;
+  virtual bool IsExtSupported(const char* extension) const = 0;
+
+  virtual void SetViewPort(const CRect& viewPort) = 0;
+  virtual void GetViewPort(CRect& viewPort) = 0;
+  virtual void RestoreViewPort() {}
+
+  virtual bool ScissorsCanEffectClipping() { return false; }
+  virtual CRect ClipRectToScissorRect(const CRect& rect) { return CRect(); }
+  virtual void SetScissors(const CRect& rect) = 0;
+  virtual void ResetScissors() = 0;
+
+  virtual void SetDepthCulling(DepthCulling culling) {}
+
+  virtual void CaptureStateBlock() = 0;
+  virtual void ApplyStateBlock() = 0;
+
+  virtual void SetCameraPosition(const CPoint &camera, int screenWidth, int screenHeight, float stereoFactor = 0.f) = 0;
+  virtual void SetStereoMode(RenderStereoMode mode, RenderStereoView view)
+  {
+    m_stereoMode = mode;
+    m_stereoView = view;
+  }
+
+  /**
+   * Project (x,y,z) 3d scene coordinates to (x,y) 2d screen coordinates
+   */
+  virtual void Project(float &x, float &y, float &z) { }
+
+  virtual std::string GetShaderPath(const std::string &filename) { return ""; }
+
+  void GetRenderVersion(unsigned int& major, unsigned int& minor) const;
+  const std::string& GetRenderVendor() const { return m_RenderVendor; }
+  const std::string& GetRenderRenderer() const { return m_RenderRenderer; }
+  const std::string& GetRenderVersionString() const { return m_RenderVersion; }
+  virtual bool SupportsNPOT(bool dxt) const;
+  virtual bool SupportsStereo(RenderStereoMode mode) const;
+  unsigned int GetMaxTextureSize() const { return m_maxTextureSize; }
+  unsigned int GetMinDXTPitch() const { return m_minDXTPitch; }
+
+  virtual void ShowSplash(const std::string& message);
+
+  /*!
+   * \brief Call when the cached advanced settings values need to be refreshed.
+   *        note: may execute on a different thread.
+   */
+  virtual void OnAdvancedSettingsLoaded();
+  virtual bool GetEnabledFrontToBackRendering();
+  virtual ClearFunction GetClearFunction();
+  virtual bool GetShowSplashImage();
+
+protected:
+  bool m_bRenderCreated{false};
+  bool m_bVSync{true};
+  unsigned int m_maxTextureSize{2048};
+  unsigned int m_minDXTPitch{0};
+
+  std::string m_RenderRenderer;
+  std::string m_RenderVendor;
+  std::string m_RenderVersion;
+  int m_RenderVersionMinor{0};
+  int m_RenderVersionMajor{0};
+  RenderStereoView m_stereoView{RenderStereoView::OFF};
+  RenderStereoMode m_stereoMode{RenderStereoMode::OFF};
+  bool m_limitedColorRange{false};
+  bool m_transferPQ{false};
+
+  std::unique_ptr<CGUIImage> m_splashImage;
+  std::unique_ptr<CGUITextLayout> m_splashMessageLayout;
+
+  // Advanced settings handling
+  CCriticalSection m_settingsSection;
+  std::optional<int> m_settingsCallbackHandle;
+  bool m_guiFrontToBackRendering{false};
+  ClearFunction m_guiGeometryClear{ClearFunction::FIXED_FUNCTION};
+  bool m_showSplashImage{true};
+};
