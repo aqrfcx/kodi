ed5ee456cd60111c834d387b60b490158dd51767
aqrfcx <sinakhorramdel.pixel@gmail.com>
2026-09-14 02:13:33 +0330

    Add boot health validation to Kodi OS image

--- os/build-iso.sh
+++ os/build-iso.sh
@@ -51,6 +51,7 @@ cp "$OS_DIR/kodi-os-recovery.service" config/includes.chroot/etc/systemd/system/
 cp "$OS_DIR/kodi-os-gaming-init.service" config/includes.chroot/etc/systemd/system/
 cp "$OS_DIR/kodi-os-controller-init.service" config/includes.chroot/etc/systemd/system/
 cp "$OS_DIR/kodi-os-network-init.service" config/includes.chroot/etc/systemd/system/
+cp "$OS_DIR/kodi-os-healthcheck.service" config/includes.chroot/etc/systemd/system/
 cp "$OS_DIR/systemd/kodi-os.target" config/includes.chroot/etc/systemd/system/
 cp "$OS_DIR/firstboot.sh" config/includes.chroot/usr/sbin/kodi-os-firstboot
 cp "$OS_DIR/kodi-os-hardware.sh" config/includes.chroot/usr/sbin/kodi-os-hardware
@@ -60,6 +61,7 @@ cp "$OS_DIR/kodi-os-storage-init" config/includes.chroot/usr/sbin/kodi-os-storag
 cp "$OS_DIR/kodi-os-gaming-init" config/includes.chroot/usr/sbin/kodi-os-gaming-init
 cp "$OS_DIR/kodi-os-controller-init" config/includes.chroot/usr/sbin/kodi-os-controller-init
 cp "$OS_DIR/kodi-os-network-init" config/includes.chroot/usr/sbin/kodi-os-network-init
+cp "$OS_DIR/kodi-os-healthcheck" config/includes.chroot/usr/sbin/kodi-os-healthcheck
 cp "$OS_DIR/kodi-os-app-launcher" config/includes.chroot/usr/sbin/kodi-os-app-launcher
 cp "$OS_DIR/includes.chroot/etc/default/grub.d/99-kodi-os.cfg" config/includes.chroot/etc/default/grub.d/99-kodi-os.cfg
 cp "$OS_DIR/configure-services.sh" config/hooks/live/0200-enable-kodi-os.hook.chroot
@@ -74,6 +76,7 @@ chmod +x config/includes.chroot/usr/sbin/kodi-os-firstboot \
   config/includes.chroot/usr/sbin/kodi-os-gaming-init \
   config/includes.chroot/usr/sbin/kodi-os-controller-init \
   config/includes.chroot/usr/sbin/kodi-os-network-init \
+  config/includes.chroot/usr/sbin/kodi-os-healthcheck \
   config/includes.chroot/usr/sbin/kodi-os-app-launcher \
   config/hooks/live/0200-enable-kodi-os.hook.chroot \
   config/hooks/live/0300-validate-kodi-os.hook.chroot \
