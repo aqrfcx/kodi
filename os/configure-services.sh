98724a023a8e40678aa72b17e0a44d082b22fe25
aqrfcx <sinakhorramdel.pixel@gmail.com>
2026-09-14 02:09:18 +0330

    Enable Kodi OS network readiness service

--- os/configure-services.sh
+++ os/configure-services.sh
@@ -5,6 +5,7 @@ systemctl enable kodi-os-firstboot.service
 systemctl enable kodi-os-session.service
 systemctl enable kodi-os-gaming-init.service
 systemctl enable kodi-os-controller-init.service
+systemctl enable kodi-os-network-init.service
 systemctl enable kodi-os-recovery.service
 systemctl enable kodi-os.target
 
